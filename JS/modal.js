0d1c350701e5f88c133b0ac97f3a55b8b6919ed3
Leoryu90 <thetein@gmail.com>
2024-05-28 00:05:32 +0900

    :memo: 20240527 homework_6

--- JS/modal.js
+++ JS/modal.js
@@ -1,8 +1,15 @@
 const goToModalButton = document.querySelector(".go_to_modal")
+const modal = document.querySelector(".modal")
+const goBackModalButton = document.querySelector(".go_back_home")
 
 function goToModal() {
+    modal.classList.add("show")
+}
 
+function goBackHome() {
+    modal.classList.remove("show")
 }
 
 
 goToModalButton.addEventListener("click", goToModal)
+goBackModalButton.addEventListener("click", goBackHome)
